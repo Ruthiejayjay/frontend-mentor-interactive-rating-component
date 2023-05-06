b7d1b32d2354df30027afc18329a8db11cc6967c
Ruth James <jamesruth449@gmail.com>
2023-05-06 18:12:24 +0100

    completed desktop and mobile state

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,20 +1,84 @@
 <template>
     <div class="w-[25.75rem] h-[26rem] bg-dark-blue rounded-2xl">
-        <div class="p-7 flex flex-col justify-start items-start gap-3 font-Overpass">
-            <div class="rounded-full flex justify-center items-center w-12 h-12 bg-grey">
+        <div class="p-7 flex flex-col justify-start items-start gap-10 font-Overpass">
+            <div class="rounded-full flex justify-center items-center w-12 h-12 bg-medium-grey">
                 <img src=".\../assets/icon-star.svg" class="w-4 h-4" />
             </div>
-            <h3 class="text-white font-bold text-xl leading-[2.216rem]">How did we do?</h3>
-            <p class="text-left text-light-grey text-[0.93rem]">Please let us know how we did with your support request. All feedback is 
-                appreciated to help us improve our offering!
-            </p>
-            <div class="grid grid-cols-5 w-[21.3rem]">
-                <div>1</div>
-                <div>1</div>
-                <div>1</div>
-                <div>1</div>
-                <div>1</div>
+            <div class="flex flex-col text-left">
+                <h3 class="text-white font-bold text-xl leading-[2.216rem]">How did we do?</h3>
+                <p class="text-left text-light-grey text-[0.93rem]">Please let us know how we did with your support request.
+                    All feedback is
+                    appreciated to help us improve our offering!
+                </p>
             </div>
+
+            <div class="grid grid-cols-5 w-[21.3rem]">
+                <div class="
+                    w-[3.188rem] 
+                    h-[3.188rem] 
+                    flex 
+                    justify-center 
+                    items-center 
+                    rounded-full
+                   text-light-grey 
+                   bg-medium-grey
+                   hover:bg-light-grey
+                   hover:text-white
+                   active:bg-orange">1
+                </div>
+                <div class="
+                    w-[3.188rem] 
+                    h-[3.188rem] 
+                    flex 
+                    justify-center 
+                    items-center 
+                    rounded-full
+                   text-light-grey 
+                   bg-medium-grey
+                   hover:bg-light-grey
+                   hover:text-white
+                   active:bg-orange">2</div>
+                <div class="
+                    w-[3.188rem] 
+                    h-[3.188rem] 
+                    flex 
+                    justify-center 
+                    items-center 
+                    rounded-full
+                   text-light-grey 
+                   bg-medium-grey
+                   hover:bg-light-grey
+                   hover:text-white
+                   active:bg-orange">3</div>
+                <div class="
+                    w-[3.188rem] 
+                    h-[3.188rem] 
+                    flex 
+                    justify-center 
+                    items-center 
+                    rounded-full
+                   text-light-grey 
+                   bg-medium-grey
+                   hover:bg-light-grey
+                   hover:text-white
+                   active:bg-orange">4</div>
+                <div class="
+                    w-[3.188rem] 
+                    h-[3.188rem] 
+                    flex 
+                    justify-center 
+                    items-center 
+                    rounded-full
+                   text-light-grey 
+                   bg-medium-grey
+                   hover:bg-light-grey
+                   hover:text-white
+                   active:bg-orange">5</div>
+            </div>
+
+            <button class="w-[21.313rem] h-[2.813rem] uppercase font-bold bg-orange text-white hover:text-orange hover:bg-white rounded-3xl">
+                Submit
+            </button>
         </div>
     </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,8 @@ module.exports = {
       'orange': 'hsl(25, 97%, 53%)',
       'white': 'hsl(0, 0%, 100%)',
       'light-grey': 'hsl(217, 12%, 63%)',
-      'medium-grey': 'hsl(216, 12%, 54%)',
-      'grey': 'hsl(213, 41%, 18%)',
+      'medium-grey': 'hsl(216, 14%, 27%, 1)',
+      'grey': 'hsl(213, 30%, 24%)',
       'dark-blue': 'hsl(213, 19%, 18%)',
       'very-dark-blue': 'hsl(216, 12%, 8%)',
       'black': 'hsl(0,0%,0%)'
